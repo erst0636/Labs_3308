23d694ba0e5707fb96b55a8aeb4d1becd004d820
erst0636 <44448517+erst0636@users.noreply.github.com>
2020-09-24 12:39:27 -0600

    Delete lab4_script.sh

--- lab4_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Authors : Erik Stolz
-# Date: 09/24/2020
-
-sudo cp /var/log/syslog ~/Documents/lab_4
-sudo grep -w 'error|ERROR|Error|' ~/Documents/lab_4/syslog |  tee ~/Documents/lab_4/error_log.txt
-mail -s 'Error log' -A ~/Documents/lab_4/error_log.txt erst0636@colorado.edu < ~/Documents/lab_4/error_log.txt
-
